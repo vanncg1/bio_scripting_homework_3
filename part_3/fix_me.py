f5b4bae6494a8b2b5b4fb3c5d92b5b72e3d50717
Christopher G. Vann <vanncg1@gmail.com>
2019-02-12 16:22:34 -0600

    corrections to script
    added float designation to a and b.
    fixed spacing and indention errors
    minor restructuring of line 73

--- part_3/fix_me.py
+++ part_3/fix_me.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
 # Reading data from the user's input
-a = input("Enter a : ")
+a = float(input ("Enter a : "))
 
 print("You entered", a, "which is a", type(a))
 
-b = input("Enter b : ")
+b = float(input("Enter b : "))
 
 print("You entered", b, "which is a", type(b))
 
@@ -32,11 +32,7 @@ quotient = a / b
 print("a / b =", quotient, ", which is a", type(quotient))
 
 floor_quotient = a // b
-print("a // b =",
-        floor_quotient,
-        ", which is a",
-        type(floor_quotient),
-        )
+print("a // b =", floor_quotient,", which is a", type(floor_quotient))
 
 remainder = a % b
 print("a % b =", remainder, ", which is a", type(remainder))
@@ -59,12 +55,12 @@ print("Now \"b\" equals", b, "and is a", type(b))
 
 
 total = a + b
- print("a + b =", total, ", which is a", type(total))
+print("a + b =", total, ", which is a", type(total))
 
 difference = a - b
 print("a - b =", difference, ", which is a", type(difference))
 
-    product = a * b
+product = a * b
 print("a * b =", product, ", which is a", type(product))
 
 quotient = a / b
@@ -74,14 +70,10 @@ floor_quotient = a // b
 print("a // b =", floor_quotient, ", which is a", type(floor_quotient))
 
 remainder = a % b
-print("a % b =," remainder ", which is a," type(remainder))
+print("a % b =", remainder, ", which is a", type(remainder))
 
 power = a ** b
-print("a ** b =",
-        power,
-        ", which is a",
-        type(power),
-        )
+print("a ** b =", power, ", which is a", type(power))
 
 a += 1
 print("Incrementing \"a\" by one results in", a, "which is a", type(a))
